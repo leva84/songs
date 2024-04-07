f10fe04aa36fb83fefb7f0226228e52ecaf57a07
DenisLevchenko <denfrut8@gmail.com>
2024-04-07 18:19:07 +0300

    Update tests for ArtistsController

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     member do
       get :songs_ordered_by_title
       get :songs_ordered_by_downloads_count
+    end
+
+    collection do
       get :top_artists_by_letter
     end
   end
