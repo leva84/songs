40b967b0629ac31a3024af2ab218596d9c80a9ba
DenisLevchenko <denfrut8@gmail.com>
2024-04-07 19:11:49 +0300

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@
 
 Rails.application.routes.draw do
   resources :artists, only: [] do
-    member do
+    collection do
       get :songs_ordered_by_title
       get :songs_ordered_by_downloads_count
-    end
-
-    collection do
       get :top_artists_by_letter
     end
   end
