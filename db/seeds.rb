6423e2788a74279d227184ed91eab294636c838d
DenisLevchenko <denfrut8@gmail.com>
2024-04-07 18:17:36 +0300

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Создание артистов
+100.times do
+  Artist.create(name: Faker::Music.band)
+end
+
+# Создание треков
+Artist.find_each do |artist|
+  rand(1..10).times do
+    artist.songs.create(title: Faker::Music.album)
+  end
+end
+
+# Создание скачиваний
+Song.find_each do |song|
+  rand(1..10).times do
+    song.downloads.create(download_date: Faker::Date.backward(days: 30))
+  end
+end
